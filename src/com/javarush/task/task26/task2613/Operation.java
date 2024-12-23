bff64cdecf3466e254d7e99c2c619bbc5bc287f3
Dyglaz <kallkae@mail.ru>
2024-12-24 01:04:57 +0300

    Add cash machine version 2

--- src/com/javarush/task/task26/task2613/Operation.java
+++ src/com/javarush/task/task26/task2613/Operation.java
@@ -1,12 +1,17 @@
 package com.javarush.task.task26.task2613;
 
 public enum Operation {
+    LOGIN,
     INFO,
     DEPOSIT,
     WITHDRAW,
     EXIT;
 
     public static Operation getAllowableOperationByOrdinal(Integer i){
+        if(LOGIN.ordinal() == i){
+            throw new IllegalArgumentException();
+        }
+
         switch (i){
             case 1:
                 return INFO;
--- src/com/javarush/task/task26/task2613/command/CommandExecutor.java
+++ src/com/javarush/task/task26/task2613/command/CommandExecutor.java
@@ -2,6 +2,7 @@ package com.javarush.task.task26.task2613.command;
 
 import com.javarush.task.task26.task2613.Operation;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,13 +14,14 @@ public class CommandExecutor {
     }
 
     static {
+        allKnownCommandsMap.put(Operation.LOGIN, new LoginCommand());
         allKnownCommandsMap.put(Operation.INFO, new InfoCommand());
         allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
         allKnownCommandsMap.put(Operation.DEPOSIT, new DepositCommand());
         allKnownCommandsMap.put(Operation.WITHDRAW, new WithdrawCommand());
     }
 
-    public static final void execute(Operation operation) throws InterruptOperationException {
+    public static final void execute(Operation operation) throws InterruptOperationException, NotEnoughMoneyException {
         Command command = allKnownCommandsMap.get(operation);
         command.execute();
     }
