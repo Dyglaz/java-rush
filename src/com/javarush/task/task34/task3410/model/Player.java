c6b961654b8437cf19439af6ae319c598bf055b5
Dyglaz <kallkae@mail.ru>
2024-12-25 02:14:10 +0300

    Add sokoban

---
+++ src/com/javarush/task/task34/task3410/model/Player.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.awt.*;
+
+public class Player extends CollisionObject implements Movable {
+
+    public Player(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.yellow);
+
+        int xc = getX();
+        int yc = getY();
+        int height = getHeight();
+        int width = getWidth();
+
+        graphics.fillOval(xc - width / 2, yc - height / 2, width, height);
+    }
+
+    @Override
+    public boolean isCollision(GameObject gameObject, Direction direction) {
+        return super.isCollision(gameObject, direction);
+    }
+
+    @Override
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+}
